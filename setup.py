284ed4d71e4e4e400fe28a19f11a106b78da6fb6
jhskone <jhskone@gmail.com>
2018-09-14 13:51:38 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='westpy',
           'datetime',
           'requests', 
           'mendeleev',
-          'signac'
+          'signac',
           'urllib3'
       ],
       python_requires='>=2.7, >=3.0, !=3.0.*, !=3.1.*, !=3.2.*, <4',
