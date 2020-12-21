0d52b644a2f3e284410adec2aa980f0370c64ac5
mahe <ted.mahe@gmail.com>
2020-12-21 12:34:10 -0600

    add point group operations to embedding/__init__.py

--- westpy/embedding/__init__.py
+++ westpy/embedding/__init__.py
@@ -1,4 +1,4 @@
 from .cgwresults import CGWResults
 from .heff import Heff
-from .symm import PointGroup
+from .symm import PointGroup, PointGroupOperation, PointGroupRotation, PointGroupReflection, PointGroupInversion
 from .west_output import WstatOutput, WfreqOutput
