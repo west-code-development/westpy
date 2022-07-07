8ba572acd5764b555327d90cbb8926ccd65f8f25
Christian Vorwerk <vorwerk@uchicago.edu>
2022-07-07 13:47:59 -0500

    Initial bugfixes. Still work to do.

--- test-suite/test_qdetresults.py
+++ test-suite/test_qdetresults.py
@@ -5,26 +5,26 @@ import numpy as np
 
 class QDETResultTestCase(unittest.TestCase):
     def setUp(self):
-        self.path = str(Path('./data').resolve)
-        self.qdetresults = QDETResults(path=path)
+        self.path = str(Path('./data').resolve())
+        self.qdetresults = QDETResults(path=self.path)
 
 
     def test_class_variables(self):
         """
         Tests QDET parameters are correct.
         """
-        self.assertEqual(self.path, str(Path('./data')))
-        self.assertEqual(self.eps_infty, None)
-        self.assertEqual(point_group, None)
-        self.assertEqual(self.nspin, 1)
-        self.assertEqual(self.npdep, 506)
-        self.assertEqual(self.omega, 4142.0863696677025)
-        self.assertEqual(self.nproj, 6)
-        self.assertEqual(self.ks_projectors, [252, 253, 254, 255, 256, 257])
-        self.assertEqual(self.basis, [0, 1, 2, 3, 4, 5])
-        self.assertEqual(self.npair, 21)
-        self.assertEqual(self.div, 0.3042077)
-        self.assertEqual(self.nel, 4)
+        self.assertEqual(self.qdetresults.path, str(Path('./data')))
+        self.assertEqual(self.qdetresults.eps_infty, None)
+        self.assertEqual(self.qdetresults.point_group, None)
+        self.assertEqual(self.qdetresults.nspin, 1)
+        self.assertEqual(self.qdetresults.npdep, 506)
+        self.assertEqual(self.qdetresults.omega, 4142.0863696677025)
+        self.assertEqual(self.qdetresults.nproj, 6)
+        self.assertEqual(self.qdetresults.ks_projectors, [252, 253, 254, 255, 256, 257])
+        self.assertEqual(self.qdetresults.basis, [0, 1, 2, 3, 4, 5])
+        self.assertEqual(self.qdetresults.npair, 21)
+        self.assertEqual(self.qdetresults.div, 0.3042077)
+        self.assertEqual(self.qdetresults.nel, 4)
 
     def test_quasiparticle_properties(self):
         """
@@ -57,7 +57,7 @@ class QDETResultTestCase(unittest.TestCase):
          -1.40564410e-04, -2.63658926e-01,  1.83579588e-04],
         [ 1.27526535e-03,  4.96289727e-08,  3.17198596e-07,
           6.92451897e-05,  1.83579588e-04, -2.64812867e-01]]])
-        sigmax_n_f_ref = np.array(array([[[-9.35359783e-01,  3.96457100e-07,  1.73876912e-07,
+        sigmax_n_f_ref = np.array([[[-9.35359783e-01,  3.96457100e-07,  1.73876912e-07,
           2.31953300e-02,  5.72247354e-03,  1.28272648e-03],
         [ 3.96457100e-07, -5.09678319e-01, -1.51202665e-02,
           3.05948917e-07,  2.64883192e-07, -3.12054836e-06],
@@ -68,16 +68,12 @@ class QDETResultTestCase(unittest.TestCase):
         [ 5.72247354e-03,  2.64883192e-07, -2.09366269e-08,
          -1.95861592e-08, -3.03374467e-01, -5.33001524e-08],
         [ 1.28272648e-03, -3.12054836e-06, -1.12010509e-07,
-          1.51572807e-07, -5.33001524e-08, -3.03375392e-01]]]))
+          1.51572807e-07, -5.33001524e-08, -3.03375392e-01]]])
 
-        np.testing.assert_almost_equal(np.diagonal(self.qp_energies),
+        np.testing.assert_almost_equal(np.diagonal(self.qdetresults.qp_energy_n[0]),
                 qp_energies_ref)
-        np.testing.assert_almost_equal(self.occ, occ_ref)
+        np.testing.assert_almost_equal(self.qdetresults.occ, occ_ref)
         
-        np.testing.assert_almost_equal(self.sigmax_n_a, sigmax_n_a_ref)
-        np.testing.assert_almost_equal(self.sigmax_n_e, sigmax_n_e_ref)
-        np.testing.assert_almost_equal(self.sigmax_n_f, sigmax_n_f_ref)
-    def test_dft_properties(self):
-        """
-        Test DFT eigenvalues.
-        """
+        np.testing.assert_almost_equal(self.qdetresults.sigmax_n_a, sigmax_n_a_ref)
+        np.testing.assert_almost_equal(self.qdetresults.sigmax_n_e, sigmax_n_e_ref)
+        np.testing.assert_almost_equal(self.qdetresults.sigmax_n_f, sigmax_n_f_ref)
