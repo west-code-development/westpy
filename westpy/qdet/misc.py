4db911d08f3da471a169f7d29321a788dd2f5516
Victor Yu <victor.wz.yu.1012@gmail.com>
2023-12-08 12:52:43 -0600

    Extend visualize_correlated_state to > 64 orbitals

--- westpy/qdet/misc.py
+++ westpy/qdet/misc.py
@@ -2,35 +2,43 @@ import numpy as np
 from pyscf.fci import cistring
 
 
-def visualize_correlated_state(evcs, norb, nelec, cutoff=10 ** (-3)):
-    """Visualizes the Slater determinants that contribute to a given many-body
-    states
+def visualize_correlated_state(evcs, norb, nelec, cutoff=1e-3):
+    """Visualizes the Slater determinants that contribute to a given many-body state
 
-    :param evcs: FCI eigenstate provided py PYSCF
+    :param evcs: FCI eigenstates provided by PySCF
     :param norb: number of orbitals that form the active space
     :param nelec: a two-entry list containing the number of spin-up and spin-down
     electrons in the active space
     :return: string representing the many-body state
     """
     # constrcut N-particle Fock space
-    string_fock = []
-    for i in range(2):
-        determinants = cistring.make_strings(range(norb), nelec[i])
-        string_fock.append(
-            ["|" + format(entry, "0" + str(norb) + "b") + ">" for entry in determinants]
-        )
+    string_fock = [[], []]
+    for ispin in range(2):
+        determinants = cistring.make_strings(range(norb), nelec[ispin])
+        for entry in determinants:
+            if norb < 64:
+                string = "|" + format(entry, "0" + str(norb) + "b") + ">"
+            else:
+                string = "|"
+                for ib in range(norb - 1, -1, -1):
+                    if ib in entry:
+                        string += "1"
+                    else:
+                        string += "0"
+                string += ">"
+            string_fock[ispin].append(string)
 
     # string for many-body state
     string = ""
-    for i in range(evcs.shape[0]):
-        for j in range(evcs.shape[1]):
-            if np.abs(evcs[i, j]) >= cutoff:
+    for ib in range(evcs.shape[0]):
+        for jb in range(evcs.shape[1]):
+            if np.abs(evcs[ib, jb]) >= cutoff:
                 string = (
                     string
-                    + format(evcs[i, j], "+4.3f")
+                    + format(evcs[ib, jb], "+4.3f")
                     + ""
-                    + string_fock[0][i]
-                    + string_fock[1][j]
+                    + string_fock[0][ib]
+                    + string_fock[1][jb]
                 )
 
     return string
